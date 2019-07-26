61a0b95a1d798ce6574f1ec4a2821ef141948307
jastring <tripp.stringfield@gmail.com>
2019-07-26 12:45:43 -0400

    added some more logic to make game function properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,15 @@
 var answerKey = ["words", "letters", "theme"];
 var randomWord;
-var hiddenWord;
+var hiddenWord = "";
 var wins = 0;
 var losses = 0;
 var userGuess;
 var guessedLetters = "";
 var numGuesses = 10;
+
+var invalidSound = new Audio("buzzer.mp3");
+var correctSound = new Audio("bell.mp3");
+
 
 var directionsText = document.getElementById("directions-text");
 var userGuessText = document.getElementById("userguess-text");
@@ -15,20 +19,61 @@ var leftText = document.getElementById("guessleft-text");
 var hiddenArray = document.getElementById("hidden-text");
 var guessArray = document.getElementById("array-text");
 
-var randomWord = answerKey[Math.floor(Math.random() * answerKey.length)];
-
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-
-    
-};
-
-
-
-
-userGuessText.textContent = "You chose: " + userGuess;
+randomWord = answerKey[Math.floor(Math.random() * answerKey.length)];
+for (i = 0; i < randomWord.length; i++) {
+    hiddenWord = hiddenWord + "_";
+}
+hiddenArray.textContent = hiddenWord;
 winsText.textContent = "wins: " + wins;
 lossesText.textContent = "losses: " + losses;
-leftText.textContent = "Guesses Left: " + numGuesses;
-hiddenArray.textContent = hiddenWord;
-guessArray.textContent = "Guessed Letters: " + guessedLetters;
+userGuessText.textContent = "You chose: " + userGuess;
+guessArray.textContent = "Incorrect Letters: " + guessedLetters;
+
+
+document.onkeyup = function (event) {
+    userGuess = event.key;
+
+    if ((hiddenWord.includes(userGuess)) || guessedLetters.includes(userGuess)) {
+        console.log("letter already guessed");
+    }
+    else {
+        if (randomWord.includes(userGuess) === false) {
+            guessedLetters = guessedLetters + userGuess;
+            numGuesses--;
+        }
+        if (randomWord.includes(userGuess)) {
+        
+            /*
+            for (i = 0; i < randomWord.length; i++) {
+                if (userGuess === randomWord[i]) {
+                    hiddenWord[i] = randomWord[i];
+                }
+            }*/
+        }
+
+
+    }
+    if (numGuesses === 0) {
+        losses++;
+        numGuesses = 10;
+        guessedLetters = "";
+        userGuess="";
+        hiddenWord="";
+        randomWord = answerKey[Math.floor(Math.random() * answerKey.length)];
+        for (i = 0; i < randomWord.length; i++) {
+            hiddenWord = hiddenWord + "_";
+        }
+        
+
+    }
+
+
+
+    userGuessText.textContent = "You chose: " + userGuess;
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    leftText.textContent = "Guesses Left: " + numGuesses;
+    hiddenArray.textContent = hiddenWord;
+    guessArray.textContent = "Incorrect Letters: " + guessedLetters;
+
+};
